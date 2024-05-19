f0b30ac349c3f7d8d85faee33505e490f1f44beb
sonicpro <chegnenado@yahoo.com>
2024-05-19 17:56:41 +0300

    The same linear regression using MathNet.Numerics package.

---
+++ Chapter 2/Simple Linear Regression/Regression/Regression/MathDotNet.fsx
@@ -0,0 +1,19 @@
+﻿let input = [| 1, 1.; 2, 2.; 3, 2.25; 4, 4.75; 5, 5 |]
+
+let axes = Array.map(fun (x, y) -> float x) input
+let yies = Array.map(fun (x, y) -> y) input
+
+#r "nuget:MathNet.Numerics, 5.0.0"
+open MathNet.Numerics.Statistics
+
+let standardDeviationX = ArrayStatistics.StandardDeviation axes
+let standardDeviationY = ArrayStatistics.StandardDeviation yies
+
+// Calculate Pearson's correlation using MathDotNet.
+let r = Correlation.Pearson (axes, yies)
+
+// Calculate the linear dependency y = A + bx regression.
+let b = r * (standardDeviationY / standardDeviationX)
+let meanX = Array.average axes
+let meanY = Array.average yies
+let A = meanY - b * meanX
