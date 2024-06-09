0c09d3abe0f4010644d7309b11feecb9cc1779e3
sonicpro <chegnenado@yahoo.com>
2024-06-09 14:49:50 +0300

    Added Multiple Linear Regression examples from the third chapter.

---
+++ Chapter 3/MultipleLinearRegression/MultipleLinearRegression/MultipleLinearRegression/AccordDotNet.fsx
@@ -0,0 +1,45 @@
+﻿#r "nuget:Accord.Statistics, 3.8.0"
+#r "nuget:Accord.Math, 3.8.0"
+
+open Accord.Statistics.Models.Regression.Linear
+open Accord.Math.Optimization.Losses
+
+// Is there a relationship between student's age, student's IQ and their GPA.
+let xs = [| [| 15.0; 130.0 |]; [| 18.0; 127.0 |]; [| 15.0; 128.0 |];
+  [| 17.0; 120.0 |]; [| 16.0; 115.0 |] |]
+// Their GPA:
+let y = [| 3.6; 3.5; 3.8; 3.4; 2.6 |]
+// Learn from the data to find the fit as an equation in the form a*x0 + b*x1 + c = y
+let ols = OrdinaryLeastSquares ()
+let regression = ols.Learn(xs, y)
+let a = regression.Weights.[0]
+let b = regression.Weights.[1]
+let c = regression.Intercept
+
+// Compute the predicted values.
+let predicted = regression.Transform(xs)
+
+// Computing how good our regression in terms of rmse and
+// Coefficient of Determination (r2).
+let mse = SquareLoss(y).Loss (predicted)
+let rmse = sqrt mse
+let r2 = regression.CoefficientOfDetermination (xs, y)
+
+// Regression of the students' GPA on age, IQ, and previous year GPA.
+let xs' = [| [| 15.0; 130.0; 3.6 |]; [| 18.0; 127.0; 3.5 |];
+  [| 15.0; 128.0; 3.7 |]; [| 17.0; 120.0; 3.5 |];
+  [| 17.0; 120.0; 2.5 |] |]
+
+let ols' = OrdinaryLeastSquares ()
+let regression' = ols.Learn(xs', y)
+let a' = regression'.Weights.[0]
+let b' = regression'.Weights.[1]
+let c' = regression'.Weights.[2]
+let d' = regression'.Intercept
+let predicted' = regression'.Transform(xs')
+let mse' = SquareLoss(y).Loss (predicted')
+let rmse' = sqrt mse'
+let r2' = regression'.CoefficientOfDetermination (xs', y)
+// For three-parameter model the rmse is 0.46, which is about 1.15% error (maximum GPA is 4).
+// r2 is about 99%, that means that we can explain 99% GPA based on
+// on a student's age, IQ, and the last year GPA.
---
+++ Chapter 3/MultipleLinearRegression/MultipleLinearRegression/MultipleLinearRegression/AccordDotNet2.fsx
@@ -0,0 +1,74 @@
+﻿#r "nuget:System.Data.SqlClient, 4.8.6"
+#r "nuget:Accord, 3.8.0"
+#r "nuget:Accord.Statistics, 3.8.0"
+
+open System.Data.SqlClient
+open Accord.Statistics.Models.Regression.Linear
+open Accord.Math.Optimization.Losses
+
+type ProductInfo = { ProductId: int; AvgOrders: float; AvgReviews: float; ListPrice: float }
+
+let productInfos = ResizeArray<ProductInfo> ()
+
+[<Literal>]
+let connectionString = "data source=(localdb)\\MSSQLLocalDB;initial catalog=AdventureWorks;integrated security=True;"
+
+let query = """
+Select 
+   A.ProductID,
+   AvgOrders,
+   AvgReviews,
+   ListPrice
+From
+    (Select 
+        ProductID,
+        (Sum(OrderQty) + 0.0)/(Count(Distinct SOH.CustomerID) + 0.0) as AvgOrders,
+        Sum(OrderQty) as TotalOrders
+    from [Sales].[SalesOrderDetail] as SOD
+        inner join [Sales].[SalesOrderHeader] as SOH
+            on SOD.SalesOrderID = SOH.SalesOrderID
+        inner join [Sales].[Customer] as C
+            on SOH.CustomerID = C.CustomerID
+    Where C.StoreID is not null
+    Group By ProductID) as A
+        Inner Join 
+    (Select
+        ProductID,
+        (Sum(Rating) + 0.0) / (Count(ProductID) + 0.0) as AvgReviews
+    from [Production].[ProductReview] as PR
+    Group By ProductID) as B
+        on A.ProductID = B.ProductID
+        Inner Join
+    (Select
+        ProductID,
+        ListPrice
+    from [Production].[Product]
+    ) as C
+        On A.ProductID = C.ProductID
+"""
+
+let connection = new SqlConnection(connectionString);
+let command = new SqlCommand(query, connection);
+connection.Open();
+let reader = command.ExecuteReader();
+while reader.Read() do
+  productInfos.Add({ ProductId = reader.GetInt32(0);
+    AvgOrders = float (reader.GetDecimal(1)); AvgReviews = float (reader.GetDecimal(2)); ListPrice = float (reader.GetDecimal(3)); });
+
+// Learn the relationship between the bike average reviews and list price and
+// its average ordered quantity by a seller customer.
+let xs = Seq.toArray (Seq.map (fun productInfo -> [| productInfo.AvgReviews;
+  productInfo.ListPrice |]) productInfos)
+let y = Seq.toArray (Seq.map (fun productInfo -> productInfo.AvgOrders) productInfos)
+
+let ols = OrdinaryLeastSquares ()
+let regression = ols.Learn(xs, y)
+// Compute the predicted values.
+let predicted = regression.Transform (xs)
+// Computing how good our regression in terms of rmse and
+// Coefficient of Determination (r2).
+let mse = SquareLoss(y).Loss (predicted)
+let rmse = sqrt mse
+let r2 = regression.CoefficientOfDetermination (xs, y)
+
+
